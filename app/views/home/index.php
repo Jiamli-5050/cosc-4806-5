c66953a19afff6313c4ac89f94ae27e6b5bce953
Jiamli-5050 <jiamli@algomau.ca>
2025-07-17 03:39:43 +0000

    updated the homepage

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -3,7 +3,7 @@
     <div class="page-header" id="banner">
         <div class="row">
             <div class="col-lg-12">
-                <h1>You are now logged in. Welcome</h1>
+                <h1>Hey! Welcome, <?= htmlspecialchars($_SESSION['username']) ?>!</h1>
             </div>
         </div>
     </div>
